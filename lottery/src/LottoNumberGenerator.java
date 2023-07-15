b07d5c98de6d61d91e396713f235dbeaf09a0a9b
karabokhunou <52717130+input-namehere@users.noreply.github.com>
2023-07-15 13:10:58 +0200

    Add files via upload

--- lottery/src/LottoNumberGenerator.java
+++ lottery/src/LottoNumberGenerator.java
@@ -2,17 +2,33 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
+/**
+ * LottoNumberGenerator class represents a lotto number generator that can generate unique random numbers
+ * within a specified range and quantity of balls.
+ */
 public class LottoNumberGenerator {
     private int minNumber;
     private int maxNumber;
     private int numberOfBalls;
 
+    /**
+     * Constructs a LottoNumberGenerator object with the specified range and quantity of balls.
+     *
+     * @param minNumber     the minimum number in the range (inclusive)
+     * @param maxNumber     the maximum number in the range (inclusive)
+     * @param numberOfBalls the quantity of balls to be selected
+     */
     public LottoNumberGenerator(int minNumber, int maxNumber, int numberOfBalls) {
         this.minNumber = minNumber;
         this.maxNumber = maxNumber;
         this.numberOfBalls = numberOfBalls;
     }
 
+    /**
+     * Generates a set of unique random lotto numbers within the specified range and quantity of balls.
+     *
+     * @return a Set of generated lotto numbers
+     */
     public Set<Integer> generateNumbers() {
         Set<Integer> generatedNumbers = new HashSet<>();
         Random random = new Random();
@@ -26,6 +42,11 @@ public class LottoNumberGenerator {
         return generatedNumbers;
     }
 
+    /**
+     * The entry point of the program.
+     *
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         int minNumber = 40;
         int maxNumber = 49;
